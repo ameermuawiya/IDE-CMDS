1d5dc427f43aef78338ac39db1c93953c22e2926
ameermuawiya <81509990+ameermuawiya@users.noreply.github.com>
2026-01-29 15:28:35 +0500

    Add Debian menu script for Android Studio setup

--- ide/androidstudio/debian.sh
+++ ide/androidstudio/debian.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
 
+clear
+echo "Debian Menu"
+echo "1. Open Debian GUI"
+echo "2. Install Android Studio"
+echo "Other. Exit"
+read -n 1 opt
+
+case "$opt" in
+1)
+    am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity
+    termux-x11 -xstartup "bash -c 'fluxbox & thunar & sleep infinity'"
+    ;;
+2)
+    bash /usr/local/bin/studio-menu.sh
+    ;;
+*)
+    exit 0
+    ;;
+esac
