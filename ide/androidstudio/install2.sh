b24cf0305f95b1c9be2f0547b1071b11764dba5e
ameermuawiya <81509990+ameermuawiya@users.noreply.github.com>
2026-01-29 13:07:00 +0500

    Enhance install script for Android Studio setup
    Added JBR link and SDK fix for Android Studio installation.

--- ide/androidstudio/install2.sh
+++ ide/androidstudio/install2.sh
@@ -1,18 +1,25 @@
 chmod -x /etc/profile.d/installstudio.sh
-clear
-echo -e '\e[1;37m[i] Installing packages...\e[0m'
+
 apt update
 apt upgrade -y
-apt install sudo xterm thunar fluxbox default-jdk android-tools-adb aapt -y
+
+apt install sudo xterm thunar fluxbox default-jdk android-tools-adb aapt git -y
+
 useradd -m devroom
 passwd -d devroom
 usermod -s /bin/bash devroom
-echo 'devroom ALL=(ALL) ALL' | tee /etc/sudoers.d/devroom
+echo 'devroom ALL=(ALL) ALL' > /etc/sudoers.d/devroom
 chmod 440 /etc/sudoers.d/devroom
-clear
-echo -e '\e[1;37m[i] Done!\e[0m'
-echo -e '\e[1;37m-\e[0m'
-echo -e "\e[1;37mUse this command to run: \"./studio.sh\"\e[0m"
-echo -e '\e[1;37m-\e[0m'
+
+# JBR link
+[ -d /Apps/IDE/android-studio ] && ln -sf /usr/lib/jvm/java-21-openjdk-arm64 /Apps/IDE/android-studio/jbr
+
+# SDK fix
+if [ -d /home/devroom/Android/Sdk/platform-tools2 ]; then
+    cd /home/devroom/Android/Sdk
+    mv platform-tools2/* platform-tools/ 2>/dev/null
+    rm -rf platform-tools2
+fi
+
+echo "Done. Use ./studio.sh"
 rm /etc/profile.d/installstudio.sh
-rm /data/data/com.termux/files/home/installstudio.sh
